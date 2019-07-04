549bedbbef95ee61b0c94cb78af2eb4e2f1c5b22
Connor Steward <Connor-Steward@users.noreply.github.com>
2019-07-05 09:56:59 +1000

    Add files via upload

---
+++ Label Printer/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Label_Printer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.generate_doc = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.labelsAvailable = new System.Windows.Forms.RichTextBox();
+            this.seperatorLine1 = new System.Windows.Forms.Label();
+            this.lableHeading = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 50);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // generate_doc
+            // 
+            this.generate_doc.Location = new System.Drawing.Point(117, 224);
+            this.generate_doc.Name = "generate_doc";
+            this.generate_doc.Size = new System.Drawing.Size(217, 62);
+            this.generate_doc.TabIndex = 49;
+            this.generate_doc.Text = "Generate .DOC";
+            this.generate_doc.UseVisualStyleBackColor = true;
+            this.generate_doc.Click += new System.EventHandler(this.generate_doc_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 86);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(322, 79);
+            this.outputBox.TabIndex = 50;
+            this.outputBox.Text = "";
+            this.outputBox.TextChanged += new System.EventHandler(this.outputBox_TextChanged);
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(181, 50);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(107, 20);
+            this.enterButton.TabIndex = 51;
+            this.enterButton.Text = "enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.Location = new System.Drawing.Point(50, 9);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(238, 24);
+            this.headerLabel.TabIndex = 52;
+            this.headerLabel.Text = "Input asset tag (PMGxxxxx)";
+            this.headerLabel.Click += new System.EventHandler(this.headerLabel_Click);
+            // 
+            // labelsAvailable
+            // 
+            this.labelsAvailable.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelsAvailable.Location = new System.Drawing.Point(12, 224);
+            this.labelsAvailable.MaxLength = 48;
+            this.labelsAvailable.Name = "labelsAvailable";
+            this.labelsAvailable.ReadOnly = true;
+            this.labelsAvailable.Size = new System.Drawing.Size(69, 62);
+            this.labelsAvailable.TabIndex = 53;
+            this.labelsAvailable.Text = "";
+            this.labelsAvailable.TextChanged += new System.EventHandler(this.labelsAvailable_TextChanged);
+            // 
+            // seperatorLine1
+            // 
+            this.seperatorLine1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.seperatorLine1.Location = new System.Drawing.Point(-53, 178);
+            this.seperatorLine1.Name = "seperatorLine1";
+            this.seperatorLine1.Size = new System.Drawing.Size(403, 2);
+            this.seperatorLine1.TabIndex = 54;
+            this.seperatorLine1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lableHeading
+            // 
+            this.lableHeading.AutoSize = true;
+            this.lableHeading.Location = new System.Drawing.Point(9, 208);
+            this.lableHeading.Name = "lableHeading";
+            this.lableHeading.Size = new System.Drawing.Size(72, 13);
+            this.lableHeading.TabIndex = 55;
+            this.lableHeading.Text = "No. Of Labels";
+            this.lableHeading.Click += new System.EventHandler(this.lableHeading_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "Published by Connor Steward V1.1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 351);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lableHeading);
+            this.Controls.Add(this.seperatorLine1);
+            this.Controls.Add(this.labelsAvailable);
+            this.Controls.Add(this.headerLabel);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.generate_doc);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PMG Label Printer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button generate_doc;
+        private System.Windows.Forms.RichTextBox outputBox;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.RichTextBox labelsAvailable;
+        private System.Windows.Forms.Label seperatorLine1;
+        private System.Windows.Forms.Label lableHeading;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Label Printer/Form1.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+// needed for http requests
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+// needed for word mail merge
+using CsvHelper;
+using word = Microsoft.Office.Interop.Word; 
+
+
+namespace Label_Printer
+{
+
+
+    public partial class Form1 : Form
+    {
+        // List that stores input PMG ID numbers
+        List<string> idNums = new List<string> { };
+        List<assetItem> csv_list = new List<assetItem>();
+
+        List<string> records = new List<string>();
+
+        // Keep track of how many labels are available
+        int label_count = 0;
+
+
+        public string getUserEntry()
+        {
+            // method to grab input ID from textBox1 
+            string userInput = textBox1.Text;
+            textBox1.Clear();
+            return userInput;
+        }
+        
+        public Form1()
+        {
+            // Forms Constructor Method
+
+            InitializeComponent();
+
+            // Binding event handler
+            // TODO: Understand what the purpose of this is
+            textBox1.KeyUp += TextBoxKeyUp;
+            outputBox.TextChanged += outputBox_TextChanged;
+            labelsAvailable.TextChanged += labelsAvailable_TextChanged;
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        // Func to generate word Doc with labels
+        private void generate_doc_Click(object sender, EventArgs e)
+        {
+
+            string path_temp = Path.GetTempPath();
+            Console.WriteLine(path_temp);
+
+            using (TextWriter writer = new StreamWriter(path_temp + @"\output.csv"))
+            {
+            var csv = new CsvWriter(writer);
+                                
+                csv.WriteField("ID");
+                csv.WriteField("Asset Tag");
+                csv.WriteField("Serial");
+                csv.NextRecord();
+                                
+                for (int i = 0; i < records.Count; i++)
+                {
+                    Console.Write(records[i]);
+                    csv.WriteField(records[i]);                                       
+
+                    if (i != 0 && (i + 1) % 3 == 0)
+                    {
+                        csv.NextRecord();                        
+                    }                       
+                }                           
+               
+                csv.NextRecord();                
+            }
+
+                       
+            
+            word.Application oWord = new word.Application();
+            word.Document oWrdDoc = new word.Document();
+            Object oTemplatePath = @"\template.docx";
+            oWrdDoc = oWord.Documents.Open(Directory.GetCurrentDirectory() + @"\template.docx");
+            oWrdDoc.MailMerge.OpenDataSource(path_temp + @"\output.csv");
+            oWrdDoc.MailMerge.Execute();
+            oWrdDoc.SaveAs2(path_temp + @"\final.docx");
+            oWrdDoc.Close();
+            oWord.Quit();
+
+
+
+        }
+
+
+        private void outputBox_TextChanged(object sender, EventArgs e)
+        {
+            // Force log/Output box to autoscroll
+            outputBox.SelectionStart = outputBox.Text.Length;
+            outputBox.ScrollToCaret();
+        }
+
+        private void TextBoxKeyUp(object sender, KeyEventArgs e)
+        {
+            // Connects the pressing of Enter to click the search button
+            if (e.KeyCode == Keys.Enter)
+            {
+                enterButton.PerformClick();
+            }
+        }
+
+        public void enterButton_Click(object sender, EventArgs e)
+        {
+            // TODO Move all the logic to it's own method/func 
+
+            // Load input into var and create regex pattern for validation
+            string id2check = getUserEntry();
+            var pmgPattern = new Regex(@"^((?i)PMG)\d\d\d\d\d(?-i)");
+
+
+            // Add to list if valid ID, update labels available display and update log
+            if (pmgPattern.IsMatch(id2check) && label_count < 47)
+            {
+                labelsAvailable.Text = String.Empty;
+                idNums.Add(id2check);
+                outputBox.SelectionColor = Color.SeaGreen;
+                outputBox.AppendText("- PCID entered: " + id2check);
+
+                string ID = "http://assets.pmgroup.com.au/hardware/" + ApiCall_id(id2check) + "/view";
+                string Serial = ApiCall_serial(id2check);
+                string Asset_Tag = id2check;
+
+
+                outputBox.AppendText(Environment.NewLine);
+                outputBox.AppendText("ID: " + ID);
+                outputBox.AppendText(Environment.NewLine);
+                outputBox.AppendText("Serial: " + Serial);
+                outputBox.AppendText(Environment.NewLine);
+
+                // Add info to dictionary or list here. This will then be used to load mail merge when 'generate doc.' function is called
+                // MOVED THIS TO TOP OF CODE -- DELETE? 
+                // Dictionary<string, string> linkList = new Dictionary<string, string>();
+                //Dictionary<string, string> serialList = new Dictionary<string, string>();
+
+                records.Add(ID);
+                records.Add(Asset_Tag);
+                records.Add(Serial);
+                               
+               
+                //This is to test info is getting stored into dict correctly 
+                // linkList.ToList().ForEach(x => Console.WriteLine(x.Value));
+                // serialList.ToList().ForEach(x => Console.WriteLine(x.Value));
+
+                label_count++;
+                labelsAvailable.AppendText(label_count.ToString());
+
+                
+            }
+
+            else
+            {
+                outputBox.SelectionColor = Color.Red;
+                outputBox.AppendText("- PCID rejected: " + id2check + " is not valid");
+                outputBox.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void labelsAvailable_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void headerLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadCSV_Click(object sender, EventArgs e)
+        {
+            LoadNewFile();
+            load_in_csv();
+
+
+        }
+
+        private void load_in_csv()
+        {
+            
+
+
+        }
+
+        private void LoadNewFile()
+        {
+            OpenFileDialog opDialog = new OpenFileDialog();
+            opDialog.Title = "Select CSV for import";
+            // TODO change to Downloads for users 
+            opDialog.InitialDirectory = @"c:\";
+            // TODO only allow csv here -- "All files (*.*)|*.*|All files (*.*)|*.*";
+            opDialog.Filter = "CSV Files (*.csv)|*.csv";
+            
+            opDialog.FilterIndex = 2;
+            opDialog.RestoreDirectory = true;
+
+            if (opDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                // test if csv loaded -- DEBUG LINE-
+                outputBox.SelectionColor = Color.SeaGreen;
+                outputBox.AppendText("- File loaded: " + opDialog.FileName);
+                
+            }
+
+        }
+
+        public string ApiCall_id(string asset_tag)
+        {
+            // 
+            // This func takes the input tag and makes a GET request to the Snipe IT REST API for the ID. It also converts the JSON resonponse so it's consumable by us later
+            //
+
+            var request = (HttpWebRequest)WebRequest.Create("https://assets.pmgroup.com.au/api/v1/hardware/bytag/" + asset_tag);
+
+            request.Method = "GET";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
+
+            // Input Snipe IT API key here
+            request.Headers["Authorization"] = "SNIPE-API-KEY-GOES-HERE";
+
+            var response = (HttpWebResponse)request.GetResponse();
+            string content = string.Empty;
+            
+            using (var stream = response.GetResponseStream())
+            {
+                using (var sr = new StreamReader(stream))
+                {
+                    content = sr.ReadToEnd();
+                    dynamic json_info = JsonConvert.DeserializeObject<dynamic>(content);
+
+                    return json_info.id;
+                                       
+                }
+            }
+        }
+
+        public string ApiCall_serial(string asset_tag)
+        {
+            // 
+            // This func takes the input tag and makes a GET request to the Snipe IT REST API for the ID. It also converts the JSON resonponse so it's consumable by us later
+            //
+
+            var request = (HttpWebRequest)WebRequest.Create("https://assets.pmgroup.com.au/api/v1/hardware/bytag/" + asset_tag);
+
+            request.Method = "GET";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
+
+            // Input Snipe IT API key here
+            request.Headers["Authorization"] = "SNIPE-API-KEY-GOES-HERE";
+
+            var response = (HttpWebResponse)request.GetResponse();
+            string content = string.Empty;
+
+            using (var stream = response.GetResponseStream())
+            {
+                using (var sr = new StreamReader(stream))
+                {
+                    content = sr.ReadToEnd();
+                    dynamic json_info = JsonConvert.DeserializeObject<dynamic>(content);
+
+                    return json_info.serial;
+
+                }
+            }
+        }
+
+        private void lableHeading_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    // CLASSES FOR JSON DESERIALIZATION
+
+    public class assetItem
+    {
+        public string serial { get; set; }
+        public string id_nums { get; set; }
+        public string asset_tag { get; set; }
+
+        public assetItem(string _serial, string _id_nums, string _asset_tag)
+        {
+            serial = _serial;
+            id_nums = _id_nums;
+            asset_tag = _asset_tag;
+
+        }
+    }
+
+
+    public class Model
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class StatusLabel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status_type { get; set; }
+        public string status_meta { get; set; }
+    }
+
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Manufacturer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class CreatedAt
+    {
+        public string datetime { get; set; }
+        public string formatted { get; set; }
+    }
+
+    public class UpdatedAt
+    {
+        public string datetime { get; set; }
+        public string formatted { get; set; }
+    }
+
+    public class PartNumber
+    {
+        public string field { get; set; }
+        public string value { get; set; }
+        public string field_format { get; set; }
+    }
+
+    public class GeneralLedger
+    {
+        public string field { get; set; }
+        public string value { get; set; }
+        public string field_format { get; set; }
+    }
+
+
+    public class AvailableActions
+    {
+        public bool checkout { get; set; }
+        public bool checkin { get; set; }
+        public bool clone { get; set; }
+        public bool restore { get; set; }
+        public bool update { get; set; }
+        public bool delete { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string asset_tag { get; set; }
+        public string serial { get; set; }
+        public Model model { get; set; }
+        public string model_number { get; set; }
+        public object eol { get; set; }
+        public StatusLabel status_label { get; set; }
+        public Category category { get; set; }
+        public Manufacturer manufacturer { get; set; }
+        public object supplier { get; set; }
+        public object notes { get; set; }
+        public string order_number { get; set; }
+        public object company { get; set; }
+        public object location { get; set; }
+        public object rtd_location { get; set; }
+        public object image { get; set; }
+        public object assigned_to { get; set; }
+        public object warranty_months { get; set; }
+        public object warranty_expires { get; set; }
+        public CreatedAt created_at { get; set; }
+        public UpdatedAt updated_at { get; set; }
+        public object last_audit_date { get; set; }
+        public object next_audit_date { get; set; }
+        public object deleted_at { get; set; }
+        public object purchase_date { get; set; }
+        public object last_checkout { get; set; }
+        public object expected_checkin { get; set; }
+        public object purchase_cost { get; set; }
+        public int checkin_counter { get; set; }
+        public int checkout_counter { get; set; }
+        public int requests_counter { get; set; }
+        public bool user_can_checkout { get; set; }
+        public AvailableActions available_actions { get; set; }
+    }
+
+
+
+
+
+}
